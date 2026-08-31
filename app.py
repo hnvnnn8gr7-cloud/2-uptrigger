e926f8a5b94c8fd4efa69578b2c966f082a45208
hnvnnn8gr7-cloud <hnvnnn8gr7@privaterelay.appleid.com>
2026-08-31 10:14:20 +0100

    Improve code formatting in app.py
    Refactor code for better readability and formatting.

--- app.py
+++ app.py
@@ -347,14 +347,14 @@ with tab3:
         for bet in bets:
 
             with st.expander(
-            f"{bet[1]} | {bet[2]}"
-         ):
+                f"{bet[1]} | {bet[2]}"
+            ):
 
-            st.write(bet)
+                st.write(bet)
 
-            st.write(
-            f"Stake: £{bet[7]}"
-        )
+                st.write(
+                f"Stake: £{bet[7]}"
+                )
 
 
                 st.write(
