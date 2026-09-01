7512635fb5f026b27a0c96526c939aba14c212bd
Mxstrd <hnvnnn8gr7@privaterelay.appleid.com>
2026-09-01 10:33:26 +0100

    Add project root to sys.path for imports
    Added project root path to sys.path for module imports.

--- training/import_2up_dataset.py
+++ training/import_2up_dataset.py
@@ -5,6 +5,21 @@ from datetime import (
     timezone
 )
 
+import sys
+from pathlib import Path
+
+PROJECT_ROOT = (
+    Path(__file__)
+    .resolve()
+    .parent
+    .parent
+)
+
+if str(PROJECT_ROOT) not in sys.path:
+    sys.path.append(
+        str(PROJECT_ROOT)
+    )
+
 from database import get_db
 
 CSV_FILE = (
